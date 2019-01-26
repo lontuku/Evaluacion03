1b40f29f5c927eff14efd575911d6362232eb5a6
Tomas K.T <tomas.konig.t@gmail.com>
2019-01-26 14:13:58 -0300

    New, create y show terminados

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,28 @@
 class TodosController < ApplicationController
+
+	def index
+		@todos = Todo.all
+	end
+
+	def new
+		@todo = Todo.new
+	end 
+
+	def create
+		@todo = Todo.new(todo_params)
+		@todo.completed = false
+		@todo.save
+		redirect_to todos_path
+	end
+
+	def show
+		@todo = Todo.find(params[:id])
+	end 
+
+	private
+	def todo_params
+		params.require(:todo).permit(:description, :completed)
+	end
+
+
 end
