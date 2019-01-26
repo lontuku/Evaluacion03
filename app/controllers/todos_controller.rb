7497e55f516e817f0b7133b4be6a034834a3e593
Tomas K.T <tomas.konig.t@gmail.com>
2019-01-26 15:23:51 -0300

    refactoring

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-
+	before_action :find_todo, only: [:show, :edit, :update, :destroy]
 	def index
 		@todos = Todo.all
 	end
@@ -16,13 +16,30 @@ class TodosController < ApplicationController
 	end
 
 	def show
-		@todo = Todo.find(params[:id])
+
 	end 
+	
+	def edit
+	
+	end
+
+	def update
+		@todo.update(todo_params)
+		redirect_to todos_path
+	end
+
+	def destroy
+		@todo.destroy
+		redirect_to todos_path
+	end
 
 	private
 	def todo_params
 		params.require(:todo).permit(:description, :completed)
 	end
 
+	def find_todo
+		@todo = Todo.find(params[:id])
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 	get 'todos/new'
 	post 'todos', to: 'todos#create'
 	get 'todos/:id', to:'todos#show', as: 'todo'
+	get 'todos/:id/edit', to:'todos#edit'
+	patch 'todos/:id', to: 'todos#update'
+	delete 'todos/:id', to: 'todos#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
